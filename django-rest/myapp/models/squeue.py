6e281cdbecdf716010462848dfb29c8bf0768304
Hengle Jiang <jianghengle@gmail.com>
2019-09-11 15:34:45 -0500

    squeue strip

--- django-rest/myapp/models/squeue.py
+++ django-rest/myapp/models/squeue.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class Squeue(object):
     def __init__(self, username):
         cmd = 'squeue -u ' + username
-        self.result = os.popen(cmd).read()
+        self.result = os.popen(cmd).read().strip()
         now = datetime.now()
         self.timestamp = datetime.timestamp(now)
 
