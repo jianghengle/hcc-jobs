5f280f9dce330864119f4b6bc71c113e12ef5e01
Hengle Jiang <jianghengle@gmail.com>
2019-10-03 11:08:02 -0500

    check lock

--- django-rest/myapp/models/my_user.py
+++ django-rest/myapp/models/my_user.py
@@ -9,11 +9,21 @@ class MyUser(object):
         ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_ALLOW)
         self.conn = ldap.initialize('ldap://hcc-ldap01.unl.edu')
         self.conn.start_tls_s()
+        self.ldap_entry = None
 
     def verify_password(self):
         status, _ = self.conn.bind_s('uid=' + self.username + ',ou=People,dc=rcf,dc=unl,dc=edu', self.password, ldap.AUTH_SIMPLE)
         if status != 97:
             raise Exception('cannot verify user')
+
+        self.get_ldap_entry()
+        if 'nsAccountLock' in self.ldap_entry.keys() and self.ldap_entry['nsAccountLock'][0] == 'true':
+            raise Exception('user has been locked')
+
+    def get_ldap_entry(self):
+        if self.ldap_entry == None:
+            results = self.conn.search_s('ou=People,dc=rcf,dc=unl,dc=edu', ldap.SCOPE_SUBTREE, 'uid=%s' % self.username, attrlist=['*'])
+            self.ldap_entry = results[0][1]
 
     def change_password(self, new_password):
         self.verify_password()
@@ -25,8 +35,7 @@ class MyUser(object):
         return result
 
     def json(self):
-        results = self.conn.search_s('ou=People,dc=rcf,dc=unl,dc=edu', ldap.SCOPE_SUBTREE, 'uid=%s' % self.username, attrlist=['*'])
-        user = results[0][1]
+        self.get_ldap_entry()
 
         cmd = 'groups ' + self.username
         ret = os.popen(cmd).read()
@@ -34,9 +43,9 @@ class MyUser(object):
 
         return {
             'username': self.username,
-            'fullname': user.get('cn', [''])[0],
-            'email': user.get('mail', [''])[0],
-            'description': user.get('description', [''])[0],
+            'fullname': self.ldap_entry.get('cn', [''])[0],
+            'email': self.ldap_entry.get('mail', [''])[0],
+            'description': self.ldap_entry.get('description', [''])[0],
             'groups': groups
         }
 
